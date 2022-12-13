9f53a039c32885739d7b0375b181ed0f4556c669
psantos1113 <psantos@synopsys.com>
2022-12-13 06:23:41 -0500

    fix: Change the destination to correct value.

--- api-distribution/src/main/java/com/synopsys/integration/alert/api/distribution/execution/JobStageEndedEvent.java
+++ api-distribution/src/main/java/com/synopsys/integration/alert/api/distribution/execution/JobStageEndedEvent.java
@@ -4,7 +4,7 @@ import java.util.UUID;
 
 public class JobStageEndedEvent extends JobStageEvent {
     private static final long serialVersionUID = -5480882643467010869L;
-    public static final String DEFAULT_DESTINATION_NAME = "job_stage_start_event";
+    public static final String DEFAULT_DESTINATION_NAME = "job_stage_end_event";
 
     public JobStageEndedEvent(UUID jobExecutionId, JobStage jobStage) {
         super(DEFAULT_DESTINATION_NAME, jobExecutionId, jobStage);
