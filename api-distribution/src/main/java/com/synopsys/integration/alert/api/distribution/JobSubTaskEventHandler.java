da11c0f0383905cce775e3730ed68e33259f4f7d
psantos1113 <psantos@synopsys.com>
2023-01-29 10:33:39 -0500

    refactor: Fire audit events like before.

--- api-distribution/src/main/java/com/synopsys/integration/alert/api/distribution/JobSubTaskEventHandler.java
+++ api-distribution/src/main/java/com/synopsys/integration/alert/api/distribution/JobSubTaskEventHandler.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import com.synopsys.integration.alert.api.common.model.exception.AlertException;
 import com.synopsys.integration.alert.api.distribution.audit.AuditFailedEvent;
+import com.synopsys.integration.alert.api.distribution.audit.AuditSuccessEvent;
 import com.synopsys.integration.alert.api.distribution.execution.JobStage;
 import com.synopsys.integration.alert.api.distribution.execution.JobStageEndedEvent;
 import com.synopsys.integration.alert.api.event.AlertEvent;
@@ -39,6 +40,7 @@ public abstract class JobSubTaskEventHandler<T extends JobSubTaskEvent> implemen
                 .filter(remainingCount -> remainingCount < 1)
                 .ifPresent(ignored -> {
                     eventManager.sendEvent(new JobStageEndedEvent(jobExecutionId, jobStage));
+                    eventManager.sendEvent(new AuditSuccessEvent(jobExecutionId, event.getNotificationIds()));
                     jobSubTaskAccessor.removeSubTaskStatus(parentEventId);
                 });
         } catch (AlertException exception) {
